29d9b02d8386a17a44337cb38621c1a634e82e38
Jacob Unknown <53351513+jacobunknown@users.noreply.github.com>
2022-10-08 19:32:10 +0100

    stuff

--- main.js
+++ main.js
@@ -1,42 +1,1 @@
-const { app, BrowserWindow, screen, nativeImage } = require("electron")
-
-if(require("electron-squirrel-startup")) {
-	app.quit()
-}
-
-app.dock.setIcon(nativeImage.createFromPath(
-	app.getAppPath() + "/Assets/icons/mac/icon.icns"
-))
-
-const createWindow = () => {
-	const {width, height} = screen.getPrimaryDisplay().workAreaSize
-	const win = new BrowserWindow({
-		width,
-		height,
-		autoHideMenuBar: true,
-		title: "Pixelful",
-		icon: "Assets/icons/win/icon.ico",
-		backgroundColor: "#222222",
-		show: false
-	})
-  
-	win.maximize()
-	win.loadFile("web/index.html")
-	win.once('ready-to-show', () => {
-		win.show()
-	})
-}
-
-app.whenReady().then(() => {
-	createWindow()
-  
-	app.on('activate', () => {
-		if (BrowserWindow.getAllWindows().length === 0) createWindow()
-	})
-})
-
-app.on('window-all-closed', () => {
-	if (process.platform == 'darwin') {
-		app.quit()
-	}
-})
+﻿const { app, BrowserWindow, screen, nativeImage } = require("electron")if(require("electron-squirrel-startup")) {	app.quit()}app.dock.setIcon(nativeImage.createFromPath(	app.getAppPath() + "/Assets/icons/mac/icon.icns"))const createWindow = () => {	const {width, height} = screen.getPrimaryDisplay().workAreaSize	const win = new BrowserWindow({		width,		height,		autoHideMenuBar: true,		title: "Pixelful",		icon: "Assets/icons/png/512x512.png",		backgroundColor: "#222222",		show: false	})  	win.maximize()	win.loadFile("web/index.html")	win.once('ready-to-show', () => {		win.show()	})}app.whenReady().then(() => {	createWindow()  	app.on('activate', () => {		if (BrowserWindow.getAllWindows().length === 0) createWindow()	})})app.on('window-all-closed', () => {	if (process.platform == 'darwin') {		app.quit()	}})
